21e9947c83158d44cb02eaee504aec200618a23f
Madhummitha <64522639+madhummitha@users.noreply.github.com>
2023-08-31 17:48:24 +0000

    Misc

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.scss';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BooksList from './components/BooksList';
+import Header from './components/Header';
+import AddBook from './components/AddBook';
+import useLocalStorage from './hooks/useLocalStorage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const App = () => {
+  const [books, setBooks] = useLocalStorage('books', [{name: "jk", age: 22}]);
+  return (
+    <>
+    <Header />
+    <Routes>
+      <Route path='/' element={<BooksList />} />
+      <Route path='/add' element={<AddBook {...{books,setBooks}} />} />
+    </Routes>
+    </>
+  )
+}
+
+const root = document.getElementById('root');
+const reactRoot = ReactDOM.createRoot(root);
+reactRoot.render(<React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </BrowserRouter>
+</React.StrictMode>);
